75f408c8b596c768a87c12dada95933cb3f3d912
Muharrem Ciftci <muharremciftci34@gmail.com>
2022-05-10 22:54:03 +0300

    mongodb added

---
+++ models/ders.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+
+const Schema=mongoose.Schema;
+
+const dersSchema=new Schema({
+    baslik:{type:String,required:true},
+    icerik:{type:String,required:true}
+}, {timestamps:true;});
+
+const Ders=mongoose.model('Derslers',dersSchema);
+
+module.exports=Ders;
+
+
